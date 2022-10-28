fbb1327318622b935b0311de913ab218db739dd4
Dev1 - filip <pelloiafilippo@live.com>
2022-10-28 11:40:42 +0200

    Commit actor

--- actor.py
+++ actor.py
@@ -2,45 +2,32 @@ from connection3 import MySql
 
 class Actor:
 
-    def getAllActors(self):
+    # def getAllActors(self):
 
-        a = MySql() 
-        a.execute(sql="SELECT * FROM Actor")
+    #     a = MySql() 
+    #     a.execute(sql="SELECT * FROM Actor")
 
-        data = a.fetchall()
-        a.closeConnection()
+    #     data = a.fetchall()
+    #     a.closeConnection()
 
-        return data  
-
-
-    def getAllActorsById(self, actorId):
-
-        a = MySql() 
-        a.execute(sql=f"SELECT first_name, last_name \
-                        FROM actor a \
-                        WHERE a.actor_id = '{actorId}'")
-
-        data = a.fetchall()
-        a.closeConnection()
-
-        return data  
+    #     return data  
 
 
-    # def getAllActorsByFilm(self, filmTitle):
+    def getAllActorsByFilm(self, filmTitle):
 
-    #     b = MySql()   ##ho l'istanza della classe MySql in a
-    #     b.execute(sql=f"SELECT first_name, last_name \
-    #                    FROM film f, film_actor fa, actor a \
-    #                    WHERE f.film_id = fa.film_id AND a.actor_id = fa.actor_id \
-    #                    AND f.title='{filmTitle}'")
+        b = MySql()   ##ho l'istanza della classe MySql in a
+        b.execute(sql=f"SELECT first_name, last_name \
+                       FROM film f, film_actor fa, actor a \
+                       WHERE f.film_id = fa.film_id AND a.actor_id = fa.actor_id \
+                       AND f.title='{filmTitle}'")
 
-    #     data = b.fetchall()
-    #     b.closeConnection() ## per chiudere la connessione
+        data = b.fetchall()
+        b.closeConnection() ## per chiudere la connessione
+
+        return data  #utile quando voglio andare a prendere i valori e richiamarli da un'altra parte come in questo caso (VEDI RIGA 30)
 
 final = Actor()
-print("Inserisci l'Id dell'attore")
-actorId = int(input())
-print(final.getAllActorsById(actorId))
+print(final.getAllActorsByFilm("AFRICAN EGG"))
 
 
 
