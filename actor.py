ea392bafb853336e6d143612d0075ca39d469d21
Dev1 - filip <pelloiafilippo@live.com>
2022-10-28 11:05:18 +0200

    Commit dev2

--- actor.py
+++ actor.py
@@ -2,32 +2,47 @@ from connection3 import MySql
 
 class Actor:
 
-    # def getAllActors(self):
+    def getAllActors(self):
 
-    #     a = MySql() 
-    #     a.execute(sql="SELECT * FROM Actor")
+        a = MySql() 
+        a.execute(sql="SELECT * FROM Actor")
 
-    #     data = a.fetchall()
-    #     a.closeConnection()
+        data = a.fetchall()
+        a.closeConnection()
 
-    #     return data  
+        return data  
 
 
-    def getAllActorsByFilm(self, filmTitle):
+    def getAllActorsById(self, actorId):
 
-        b = MySql()   ##ho l'istanza della classe MySql in a
-        b.execute(sql=f"SELECT first_name, last_name \
-                       FROM film f, film_actor fa, actor a \
-                       WHERE f.film_id = fa.film_id AND a.actor_id = fa.actor_id \
-                       AND f.title='{filmTitle}'")
+        a = MySql() 
+        a.execute(sql=f"SELECT first_name, last_name \
+                        FROM actor a \
+                        WHERE a.actor_id = '{actorId}'")
 
-        data = b.fetchall()
-        b.closeConnection() ## per chiudere la connessione
+        data = a.fetchall()
+        a.closeConnection()
 
-        return data  #utile quando voglio andare a prendere i valori e richiamarli da un'altra parte come in questo caso (VEDI RIGA 30)
+        return data  
+
+
+    # def getAllActorsByFilm(self, filmTitle):
+
+    #     b = MySql()   ##ho l'istanza della classe MySql in a
+    #     b.execute(sql=f"SELECT first_name, last_name \
+    #                    FROM film f, film_actor fa, actor a \
+    #                    WHERE f.film_id = fa.film_id AND a.actor_id = fa.actor_id \
+    #                    AND f.title='{filmTitle}'")
+
+    #     data = b.fetchall()
+    #     b.closeConnection() ## per chiudere la connessione
+
+    #     return data  #utile quando voglio andare a prendere i valori e richiamarli da un'altra parte come in questo caso (VEDI RIGA 30)
 
 final = Actor()
-print(final.getAllActorsByFilm("AFRICAN EGG"))
+print("Inserisci l'Id dell'attore")
+actorId = int(input())
+print(final.getAllActorsById(actorId))
 
 
 
